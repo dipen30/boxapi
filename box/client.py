8cedda155eb6f66e19eb5da4cbd02c9bafefb078
Dipen Patel <patel.dipen30@gmail.com>
2013-09-14 11:16:39 +0530

    - File upload API added(tested with small file size only)

--- box/client.py
+++ box/client.py
@@ -6,12 +6,13 @@ import urllib
 import httplib
 import json
 
-from error import ERRORCODES
+from error import ERRORCODES, BoxError
+import mimetypes
 
 BOX_API_VERSION = "/2.0"
 BOX_API_URL = "api.box.com"
 BOX_DOWNLOAD_URL = "dl.boxcloud.com"
-BOX_API_UPLOAD_URL = "https://upload.box.com/api/2.0"
+BOX_API_UPLOAD_URL = "upload.box.com"
 
 
 class BoxClient(object):
@@ -75,7 +76,17 @@ class BoxClient(object):
         return self.request("/folders/", method='POST', post_args=post_data)
 
     def update_folder_info(self, folderId, **post_data):
-        """update folder information"""
+        """update folder information
+        
+        Args:
+            - folderId : folder's id to update information
+            - post_data : parameter lists to update(in json format)
+            
+        Returns:
+            - The updated folder is returned if the name is valid
+            for more details, visit:
+            http://developers.box.com/docs/#folders-update-information-about-a-folder
+        """
         return self.request("/folders/"+folderId, method="PUT", post_args=post_data)
 
     def delete_folder(self, folderId, **qs_args):
@@ -116,6 +127,102 @@ class BoxClient(object):
             close HttpResponse once file is downloaded.
         """
         return self.request("/files/"+fileId+"/content", qs_args=args)
+    
+    def upload_file(self, fileObj, parentId, fileId=None):
+        """ upload a file to specified folder
+        
+        Args:
+            - fileObj : file object
+            - parentId : folder id where file need to upload
+            - fileId : if wanted to update existing file in parentId
+        
+        Returns:
+            - full file object is returned in json object if the ID is valid
+            for more details, visit
+            http://developers.box.com/docs/#files-upload-a-file
+        """
+        return self.request_upload(parentId, fileObj=fileObj, fileId=fileId)
+
+    def request_upload(self, parentId, method='POST', fileObj=None, fileId=None):
+        """An internal method that builds the url, headers, and params for Box API request.
+        
+        Args:
+            - path : API endpoint with leading slash
+            - method : An HTTP method
+            - qs_args : query sting arguments to send
+            - post_args : POST data to send
+        
+        Returns:
+            - return json or raw response based on API endpoint.
+        """
+        
+        con = httplib.HTTPSConnection(BOX_API_UPLOAD_URL, timeout=self.timeout)
+        
+        if fileId:
+            path = '/api/'+BOX_API_VERSION+'/files/'+fileId+'/content'
+        else:    
+            path = '/api/'+BOX_API_VERSION+'/files/content'
+
+        headerValue = 'Bearer %s' % (self.access_token,)
+        fields = {"filename": fileObj,"parent_id":parentId}
+        content_type, body = self._encode_multipart_form(fields)
+
+        headers = {
+                   "Authorization": headerValue,
+                   "Content-Type": content_type
+                   }
+
+        con.request(method, path, body, headers)
+        response = {}
+
+        data = con.getresponse()
+        if data.status in ERRORCODES:
+            response["status"] = data.status
+            response["error"] = data.reason
+        else:
+            response = data.read()
+        data.close()
+        con.close()
+
+        try:
+            return json.loads(response)
+        except Exception, e:
+            raise BoxError(e)
+
+    # based on: http://code.activestate.com/recipes/146306/
+    def _encode_multipart_form(self, fields):
+        """Encode files as 'multipart/form-data'.
+
+        Fields are a dict of form name-> value. For files, value should
+        be a file object. Other file-like objects might work and a fake
+        name will be chosen.
+
+        Returns (content_type, body) ready for httplib.HTTP instance.
+
+        """
+        BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+        CRLF = '\r\n'
+        L = []
+        for (key, value) in fields.items():
+            L.append('--' + BOUNDARY)
+            if hasattr(value, 'read') and callable(value.read):
+                filename = getattr(value, 'name')
+                L.append(('Content-Disposition: form-data;'
+                          'name="%s";'
+                          'filename="%s"') % (key, filename))
+                L.append('Content-Type: %s' % (mimetypes.guess_type(filename)[0],))
+                value = value.read()
+            else:
+                L.append('Content-Disposition: form-data; name="%s"' % key)
+            L.append('')
+            if isinstance(value, unicode):
+                value = value.encode('ascii')
+            L.append(value)
+        L.append('--' + BOUNDARY + '--')
+        L.append('')
+        body = CRLF.join(L)
+        content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+        return content_type, body
 
     def request(self, path, method='GET', qs_args=None, post_args=None):
         """An internal method that builds the url, headers, and params for Box API request.
